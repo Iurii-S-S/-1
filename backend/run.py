c1a2ab7f976db4b5d8fd90efb230833e8084d660
Iurii-S-S <>
2025-10-08 21:32:57 +0300

    Полностью переделанный сайт только с фронтом и local storage, без бэка и бд (как сказали на лекции)

--- backend/run.py
+++
@@ -1,9 +0,0 @@
-from app import create_app
-
-app = create_app()
-
-if __name__ == '__main__':
-    print("=" * 50)
-    print("🚀 Defect Management API Server")
-    print("=" * 50)
-    app.run(debug=True, host='0.0.0.0', port=5000)
